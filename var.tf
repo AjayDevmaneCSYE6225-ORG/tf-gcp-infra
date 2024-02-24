ecb91b1e175c47bd485931b95d22be2f09837e9c
ajaysubashdevmane <ajaysubashdevmane@Hephaestus.local>
2024-02-24 14:45:47 -0500

    ta trial

--- var.tf
+++ var.tf
@@ -7,33 +7,63 @@ variable "projectId" {
 }
 
 variable "region" {
-  default     = "us-west2"
+  default = "us-west2"
+}
+
+variable "zone" {
+  default = "us-west2-a"
 }
 
 variable "webappSubnetCidr" {
-  default     = "10.0.0.0/24"
+  default = "10.0.0.0/24"
 }
 
 variable "dbSubnetCidr" {
-   default     = "10.0.1.0/24"
+  default = "10.0.1.0/24"
 }
 
 variable "num_vpcs" {
   description = "Number of VPCs to create"
-  default="1"
+  default     = "1"
 }
 
 variable "num_webapp_subnets_per_vpc" {
   description = "Number of subnets to create per VPC"
-  default="1"
+  default     = "1"
 }
 
 variable "num_db_subnets_per_vpc" {
   description = "Number of subnets to create per VPC"
-  default="1"
+  default     = "1"
 }
 
 variable "port" {
   description = "Port Number"
-  default="3000"
+  default     = "3000"
+}
+
+variable "routing_mode" {
+  description = "Routing Mode"
+  default     = "REGIONAL"
+}
+
+variable "dest_range" {
+  description = "Destination range"
+  default     = "0.0.0.0/0"
+}
+
+variable "next_hop_gateway" {
+  default = "default-internet-gateway"
+}
+
+variable "address_type" {
+  default = "EXTERNAL"
+}
+
+variable "ip_version" {
+  default = "IPV4"
+}
+
+variable "machine_type" {
+  default = "n2-standard-2"
 }
