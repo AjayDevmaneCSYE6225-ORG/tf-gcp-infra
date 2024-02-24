6109b71d2084aae438c14866b26cd17727f43f9e
ajaysubashdevmane <ajaysubashdevmane@Hephaestus.local>
2024-02-24 14:45:47 -0500

    ta trial

--- main.tf
+++ main.tf
@@ -40,91 +40,89 @@ provider "google" {
   region      = var.region
 }
 
-locals {
-  vpc_names = [for i in range(var.num_vpcs) : "csye-vpc-${i}"]
-}
-
 resource "google_compute_network" "vpc" {
-  count                   = var.num_vpcs
-  name                    = local.vpc_names[count.index]
-  auto_create_subnetworks = false
-  routing_mode            = "REGIONAL"
+
+  name                            = "csye-vpc"
+  auto_create_subnetworks         = false
+  routing_mode                    = var.routing_mode
   delete_default_routes_on_create = true
 }
 
 resource "google_compute_subnetwork" "webapp_subnet" {
-  count         = var.num_vpcs * var.num_webapp_subnets_per_vpc
-  name          = "webapp-${count.index}"
+
+  name          = "csye-webapp-subnet"
   region        = var.region
-  network       = google_compute_network.vpc[floor(count.index / var.num_webapp_subnets_per_vpc)].self_link
-  ip_cidr_range = "10.${count.index}.0.0/24"
+  network       = google_compute_network.vpc.self_link
+  ip_cidr_range = var.webappSubnetCidr
 }
 
 resource "google_compute_subnetwork" "db_subnet" {
-  count         = var.num_vpcs * var.num_db_subnets_per_vpc
-  name          = "db-${count.index}"
+
+  name          = "csye-db"
   region        = var.region
-  network       = google_compute_network.vpc[floor(count.index / var.num_db_subnets_per_vpc)].self_link
-  ip_cidr_range = "10.${count.index + 100}.0.0/24"
+  network       = google_compute_network.vpc.self_link
+  ip_cidr_range = var.dbSubnetCidr
 }
 
 resource "google_compute_route" "webapp_route" {
-  count                 = var.num_vpcs
-  name                  = "webapp-route-${count.index}"
-  network               = google_compute_network.vpc[count.index].self_link
-  dest_range            = "0.0.0.0/0"
-  next_hop_gateway      = "default-internet-gateway"
-  priority              = 1000
-  tags                  = ["webapp"]
+
+  name             = "webapp-route"
+  network          = google_compute_network.vpc.self_link
+  dest_range       = var.dest_range
+  next_hop_gateway = var.next_hop_gateway
+  priority         = 1000
+  tags             = ["webapp"]
+}
+
+resource "google_compute_address" "default" {
+  project      = var.projectId
+  name         = "ipv4-address"
+  address_type = var.address_type
+  ip_version   = var.ip_version
 }
 
 resource "google_compute_instance" "default" {
   name         = "csye-instance"
-  machine_type = "n2-standard-2"
-  zone         = "us-central1-a"
+  machine_type = var.machine_type
+  zone         = var.zone
 
   boot_disk {
     initialize_params {
-      image = "projects/debian-cloud/global/images/debian-12-bookworm-v20240213"
+      image = "projects/csye6225-414121/global/images/my-custom-image"
       size  = 100
       type  = "pd-balanced"
     }
   }
 
   network_interface {
-    network = "default"
+    network    = google_compute_network.vpc.self_link
+    subnetwork = google_compute_subnetwork.webapp_subnet.self_link
+    access_config {
+      nat_ip = google_compute_address.default.address
+    }
   }
 
   service_account {
-
     email  = "packer-buildmi-sa@csye6225-414121.iam.gserviceaccount.com"
     scopes = ["cloud-platform"]
   }
+
+  tags = ["webapp"]
+
 }
 
-resource "google_compute_firewall" "allowHttp" {
-  count   = var.num_vpcs
-  name    = "allow-http-${count.index}"
-  network = google_compute_network.vpc[count.index].name
+resource "google_compute_firewall" "firewallsRules" {
 
+  name    = "firewall-rule"
+  network = google_compute_network.vpc.self_link
   allow {
     protocol = "tcp"
-    ports    = ["${var.port}"] 
+    ports    = [var.port, "22"]
   }
 
-  source_ranges = ["0.0.0.0/0"]
+  priority  = 1000
+  direction = "INGRESS"
+
+  source_ranges = [var.dest_range, "35.235.240.0/20"]
   target_tags   = ["webapp"]
-}
-
-resource "google_compute_firewall" "denySsh" {
-  count   = var.num_vpcs
-  name    = "deny-ssh-${count.index}"
-  network = google_compute_network.vpc[count.index].name
-
-  deny {
-    protocol = "tcp"
-    ports    = ["22"]
-  }
-
-  source_ranges = ["0.0.0.0/0"]
 }
